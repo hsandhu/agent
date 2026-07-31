cc1a404e2f7d1453d0a34072d40e2bacf563ca81
Rob Sandhu <sandhu.rob@gmail.com>
2026-07-31 16:32:56 -0700

    Initial Commit

---
+++ Agent/AgentApp.swift
@@ -0,0 +1,32 @@
+//
+//  AgentApp.swift
+//  Agent
+//
+//  Created by Harminder Sandhu on 2026-07-31.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AgentApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
